5ce18fdcade505159568b89c543c318533b3cc6e
Bindu <bindusree.vemulakonda05@gmail.comgit config --global user.email bindusree.vemulakonda05@email.com>
2025-07-16 12:57:11 +0530

    initial21

---
+++ hey.py
@@ -0,0 +1,29 @@
+name = "Bindu"
+age = 20
+is_python_beginner = True
+print("My name is", name + ".")
+print("I am", age, "years old.")
+print("Beginner in Python:", is_python_beginner)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
