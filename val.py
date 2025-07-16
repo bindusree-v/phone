a71b2692693f56b6c1bd53786516e1ba68ab8320
Bindu <bindusree.vemulakonda05@gmail.comgit config --global user.email bindusree.vemulakonda05@email.com>
2025-07-16 12:40:45 +0530

    initial

---
+++ val.py
@@ -0,0 +1,31 @@
+# 123=10
+# print(123)
+
+
+
+# @test ="hello"
+# print(@test)
+
+_xyz2 = 3.14
+print(_xyz)
+
+   # iv){{{ = [1, 2, 3] 
+   #    print({{{)
+
+   # v)True = False
+   #   print(True)
+
+   # vi)my.name = "Alice" 
+   #    print(my.name)
+
+   # vii)break = 10
+   #     print(break)
+
+   # viii)1st_place = "Gold 
+   #      print(1st_place)
+
+   # ix)$price = 99.99 
+   #    print($price)
+
+   # x)my-var = "test" 
+   #   print(my-var)*/
