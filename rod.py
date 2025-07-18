7674be31a1247f6ce80e878f1bb4140bae2137f3
Bindu <bindusree.vemulakonda05@gmail.comgit config --global user.email bindusree.vemulakonda05@email.com>
2025-07-18 12:28:42 +0530

    onedfgh

---
+++ rod.py
@@ -0,0 +1,31 @@
+# 123=10
+# print(123)
+
+jfjfjfjjff
+
+# @test ="hello"
+# print(@test)
+hh
+_xyz2 = 3.14j
+print(_xyz)
+
+   # iv){{{ = [1, 2, 3] 
+   #    print({{{)
+
+   # v)True = False
+   #   print(True)
+
+   # vi)my.name = "Alice" 
+   #    print(my.name)
+
+   # vii)break = 10
+   #     print(break)
+
+   # viii)1st_place = "Gold 
+   #      print(1st_place)
+
+   # ix)$price = 99.99 
+   #    print($price)
+
+   # x)my-var = "test" 
+   #   print(my-var)*/
